96157d8a57f5129efc0c27c0fba024c8c7f94d2e
jonatansuriani <jonatansuriani@yahoo.com.br>
2016-04-10 16:33:37 -0300

    Solution upload

---
+++ ValveKeyValues2Json/KeyValuesConverter.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ValveKeyValues2Json
+{
+    public class KeyValuesConverter
+    {
+        public static string ToJson(string KeyValues)
+        {
+            var json = KeyValues;
+
+            // remove comments: /* */ and // (double dash)
+            json = Regex.Replace(json, @"(@(?:""[^""]*"")+|""(?:[^""\n\\]+|\\.)*""|'(?:[^'\n\\]+|\\.)*')|//.*|/\*(?s:.*?)\*/", "$1");
+            // remove comments: /* */ and / (single dash
+            json = Regex.Replace(json, @"(@(?:""[^""]*"")+|""(?:[^""\n\\]+|\\.)*""|'(?:[^'\n\\]+|\\.)*')|/.*|/\*(?s:.*?)\*/", "$1");
+
+            //complement ":" before open braces
+            json = Regex.Replace(json, "\"([^\"]*)\"(\\s*){", "\"${1}\": {");
+
+            // place ":" between key/value
+            json = Regex.Replace(json, "\"([^\"]*)\"\\s*\"([^\"]*)\"", "\"${1}\": \"${2}\",");
+
+            //remove trailing commas
+            json = Regex.Replace(json, ",(\\s*[}\\]])", "${1}");
+
+            //add commas
+            json = Regex.Replace(json, "([}\\]])(\\s*)(\"[^\"]*\":\\s*)?([{\\[])", "${1},${2}${3}${4}");
+
+            //object as value
+            json = Regex.Replace(json, "}(\\s*\"[^\"]*\":)", "},${1}");
+
+            return string.Concat("{\n", json, "\n}");
+        }
+    }
+}
---
+++ ValveKeyValues2Json/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+namespace ValveKeyValues2Json
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = string.Concat(Directory.GetCurrentDirectory(), "\\files\\");
+
+            var npc_heroes = KeyValuesConverter.ToJson(File.ReadAllText(string.Concat(path, "npc_heroes.txt")));
+            var npc_abilities = KeyValuesConverter.ToJson(File.ReadAllText(string.Concat(path, "npc_abilities.txt")));
+            var items = KeyValuesConverter.ToJson(File.ReadAllText(string.Concat(path, "items.txt")));
+        }
+    }
+}
